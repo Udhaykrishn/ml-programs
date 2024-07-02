999399a4b1170dbd6a87476ab42a16c82589dfaa
Udhaykrishn <krishnauday320@gmail.com>
2024-07-02 11:47:20 +0530

    first program is done

--- first/index.py
+++ first/index.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import pandas as pd
+
+
+csv_file = r"C:\Users\krish\OneDrive\Desktop\python\ml\sample_data.csv"
+
+
+data_csv = pd.read_csv(csv_file)
+print("CSV file data: ")
+print(data_csv)
+
+print("\nData Dscriptoin")
+print("CSV data Dscriptoin")
+print(data_csv.describe())
+
